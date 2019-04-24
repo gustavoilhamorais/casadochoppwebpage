00820f09b3193fd4f894ae0aa037f8a3c6161617
CyberGust <gustavoilhamorais@gmail.com>
2019-04-24 16:37:50 -0300

    backend ok

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
+const hbs = require('express-handlebars');
+const path = require('path');
 
+// Public //
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handlebars //
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'index' }));
+
+// Require modules //
+const slash = require('./routes/slash');
+
+// Routes //
+app.use('/', slash);
 
 // Express Server //
 const PORT = 7001;
